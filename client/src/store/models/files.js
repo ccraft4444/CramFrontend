68c341d36e6466de5863e90c8fd2654c78d54527
Carson Craft <ccraft4444@gmail.com>
2023-09-11 18:00:26 -0700

    change env var name

--- client/src/store/models/files.js
+++ client/src/store/models/files.js
@@ -7,13 +7,15 @@ export const files = {
     state.data = payload;
   }),
   fetchFiles: thunk(async (actions, payload) => {
-    const { data } = await axios.get(`${process.env.DB_URL}/routes/documents`);
+    const { data } = await axios.get(
+      `${process.env.VITE_DB_URL}/routes/documents`
+    );
     actions.setFIle(data);
   }),
   fetchFile: action(async (state, fileId) => {
     try {
       const response = await fetch(
-        `${process.env.DB_URL}/routes/documents/${fileId}`
+        `${process.env.VITE_DB_URL}/routes/documents/${fileId}`
       );
       const blob = await response.blob();
       return blob;
@@ -25,7 +27,7 @@ export const files = {
   createFile: thunk(async (actions, payload) => {
     console.log("create file payload", payload);
     const { data } = await axios.post(
-      `${process.env.DB_URL}/routes/documents`,
+      `${process.env.VITE_DB_URL}/routes/documents`,
       payload
     );
     actions.setFile(data);
--- client/src/store/models/orders.js
+++ client/src/store/models/orders.js
@@ -8,12 +8,14 @@ export const orders = {
   }),
   fetchOrders: thunk(async (actions, payload) => {
     const { data } = await axios.get(
-      `${process.env.DB_URL}/routes/users/my_orders`
+      `${process.env.VITE_DB_URL}/routes/users/my_orders`
     );
     actions.setOrder(data);
   }),
   createOrder: thunk(async (actions, payload) => {
-    const { data } = await axios.post(`${process.env.DB_URL}/routes/orders`);
+    const { data } = await axios.post(
+      `${process.env.VITE_DB_URL}/routes/orders`
+    );
     actions.setOrder(data);
   }),
 };
